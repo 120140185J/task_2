0a4bbe45fb3dae87d21cd1660cd7a093410e578a
120140185J <justin.120140185@student.itera.ac.id>
2022-10-07 01:39:15 +0700

    Initial commit

---
+++ action_page.php
@@ -0,0 +1,7 @@
+</head>
+<body>
+
+Nama responden <?php echo $_GET["nama"]; ?> dengan NIM <?php echo $_GET["nim"]; ?> dari jurusan <?php echo $_GET["jurusan"]; ?> menyatakan : <br/> <?php echo $_GET["saran"]; ?>
+
+</body>
+</html>
